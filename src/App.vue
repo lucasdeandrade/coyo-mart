c5f989678cd37ae0e6f7f503a46ed95e9d100538
lucasdeandrade <lucasdeandrade.dev@gmail.com>
2023-02-01 20:14:39 -0300

    essential files and searching api products

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/products">Produtos</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+
+  export default {
+
+    created() {
+      this.$store.dispatch('loadProducts')
+    }
+
+  }
+</script>
 
 <style lang="scss">
 #app {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,25 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: []
   },
   getters: {
   },
   mutations: {
+    loadProducts(state, products){
+      state.products = products;
+    }
   },
   actions: {
+    loadProducts({commit}){
+        axios
+          .get('http://127.0.0.1:3000/products')
+          .then(response => {
+            commit('loadProducts', response.data);
+          })
+    }
   },
   modules: {
   }
